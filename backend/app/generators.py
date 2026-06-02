b87322d48df2af86d813104f9d809afb7ebcc873
Maciej Kot <mackot1337@gmail.com>
2026-06-02 12:21:04 +0200

    feat(logo): add pasting logo on qr code feature

--- backend/app/generators.py
+++ backend/app/generators.py
@@ -17,7 +17,7 @@ class CodeGenerator(ABC):
 
 
 class QRCodeGenerator(CodeGenerator):
-    def generate(self, data: str, fill_color: str = "black", back_color: str = "white", **kwargs) -> str:
+    def generate(self, data: str, fill_color: str = "black", back_color: str = "white", logo_base64: str = None,**kwargs) -> str:
 
         qr = qrcode.QRCode(
             version=1,
@@ -28,8 +28,29 @@ class QRCodeGenerator(CodeGenerator):
         qr.add_data(data)
         qr.make(fit=True)
 
-        img = qr.make_image(fill_color=fill_color, back_color=back_color)
-        
+        img = qr.make_image(fill_color=fill_color, back_color=back_color).convert("RGB")
+
+        if logo_base64:
+            try:
+                if "," in logo_base64:
+                    logo_base64 = logo_base64.split(",")[1]
+                        
+                logo_bytes = base64.b64decode(logo_base64)
+                logo = Image.open(BytesIO(logo_bytes))
+                
+                if logo.mode != 'RGBA':
+                    logo = logo.convert('RGBA')
+
+                basewidth = int(img.size[0] / 4)
+                wpercent = (basewidth / float(logo.size[0]))
+                hsize = int((float(logo.size[1]) * float(wpercent)))
+                logo = logo.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+                
+                pos = ((img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
+                img.paste(logo, pos, mask=logo)
+            except Exception as e:
+                print(f"Błąd podczas wklejania logo: {e}")
+
         buffer = BytesIO()
         img.save(buffer, format="PNG")
         return self._encode_to_base64(buffer)
